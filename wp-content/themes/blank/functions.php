ca2ff1b25f26e95415a00e7d236428dad98e6b52
Don Leistman <donleistman@gmail.com>
2018-10-12 15:59:35 -0400

    add code to functions.php in wordpress theme to have ACF fields appear in JSON API

---
+++ wp-content/themes/blank/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+$post_type = "painting";
+
+function my_rest_prepare_post($data, $post, $request) {
+    $_data = $data->data;
+
+    $fields = get_fields($post->ID);
+
+    foreach ($fields as $key => $value){
+        $_data[$key] = get_field($key, $post->ID);
+    }
+
+    $data->data = $_data;
+    return $data;
+}
+
+add_filter("rest_prepare_{$post_type}", 'my_rest_prepare_post', 10, 3);
+
+?>
