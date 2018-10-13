f9f175009cd5a6c92f1331158b4c3d00cb511e07
Don Leistman <donleistman@gmail.com>
2018-10-13 01:12:48 -0400

    change localhost port in redirect

--- wp-content/themes/blank/index.php
+++ wp-content/themes/blank/index.php
@@ -2,5 +2,5 @@
 
 <!-- just in case the meta tag is not read properly, here is plan B: a JS redirect -->
 <script type="text/javascript">
-  window.location = 'http://localhost:8000';
+  window.location = 'http://localhost:8080';
 </script>
