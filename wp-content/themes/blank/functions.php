4aa73e39a3663fbfc1d2ab7f759fa52e621f3962
Don Leistman <donleistman@gmail.com>
2018-10-15 11:03:22 -0400

    add back code to put ACF fields at top level of objects returned by REST api

--- wp-content/themes/blank/functions.php
+++ wp-content/themes/blank/functions.php
@@ -1,5 +1,26 @@
 <?php
 
+// GOT THIS ONLINE TO SHOW ACF FIELDS -- MAY NOT ACTUALLY BE NECESSARY
+
+$post_type = "painting";
+
+function my_rest_prepare_post($data, $post, $request) {
+    $_data = $data->data;
+
+    $fields = get_fields($post->ID);
+
+    foreach ($fields as $key => $value){
+        $_data[$key] = get_field($key, $post->ID);
+    }
+
+    $data->data = $_data;
+    return $data;
+}
+
+add_filter("rest_prepare_{$post_type}", 'my_rest_prepare_post', 10, 3);
+
+//// END CODE FROM ONLINE FOR ACF FIELDS
+
 /*
  * Add columns to writing post list
  */
@@ -25,5 +46,6 @@ function writing_custom_column ( $column, $post_id ) {
   }
 }
 add_action ( 'manage_writing_posts_custom_column', 'writing_custom_column', 10, 2 );
+
 
 ?>
